cac4a17c0543677ef06e2e502234350ac3a6e1c0
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-05 01:48:38 +0600

    task1

--- Tasks/Task_1.java
+++ Tasks/Task_1.java
@@ -2,6 +2,34 @@ package Tasks;
 
 public class Task_1 {
     public static void main(String[] args) {
-        System.out.println("H");
+        // A
+        int var = 5;
+        System.out.println("Integer Variable: " + var);
+        // B
+        int x = 3;
+        var += x;
+        System.out.println("Addition: " + var);
+        // C
+        var = 5; // initializing var again, as my decladed variable was 5
+        System.out.println("Product: " + var * x);
+        System.out.println("Division: " + var * 1.0 / x);
+        // D
+        double varD = 5.0;
+        System.out.println("Double Variable: " + varD);
+        double xD = 3.0;
+        varD += xD;
+        System.out.println("Addition: " + varD);
+        varD = 5.0; // initializing var again, as my decladed variable was 5.0
+        System.out.println("Product: " + varD * xD);
+        System.out.println("Division: " + varD / xD);
+        // E
+        double var_D = 5.0;
+        System.out.println("Double Variable: " + var_D);
+        int xI = 3;
+        var_D += xI;
+        System.out.println("Addition: " + var_D);
+        var_D = 5; // initializing var again, as my decladed variable was 5
+        System.out.println("Product: " + var_D * xI);
+        System.out.println("Division: " + var_D / xI);
     }
 }
