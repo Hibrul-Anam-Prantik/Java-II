e38abd9922068c014568e101e9cbe33824cdb913
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-08 00:07:34 +0600

    task 3 added

--- Tasks/Task_3.java
+++ Tasks/Task_3.java
@@ -1,1 +1,16 @@
+package Tasks;
 
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int id = sc.nextInt();
+        for (int i = 0; i <= 1; i++) {
+            int digit = id % 10;
+            id /= 10;
+            System.out.println(digit);
+        }
+        sc.close();
+    }
+}
