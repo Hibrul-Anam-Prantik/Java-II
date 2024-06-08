7e5b49f70c83b1b2de449bbf3bf4b76a674929b4
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-08 16:16:26 +0600

    Task 6 added

--- Tasks/Task_5.java
+++ Tasks/Task_5.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Task_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-        if (n1 > n2 && n1 > n3) {
-            System.out.println("Largest number: " + n1);
-        } else if (n2 > n1 && n2 > n3) {
-            System.out.println("Largest number: " + n2);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Largest number: " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("Largest number: " + num2);
         } else {
-            System.out.println("Largest number: " + n3);
+            System.out.println("Largest number: " + num3);
         }
         sc.close();
     }
