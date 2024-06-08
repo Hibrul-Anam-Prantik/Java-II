8e14c478bc4095d0c913bf672ca117c3792002e3
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-08 15:46:37 +0600

    Task 4 added

--- Tasks/Task_4.java
+++ Tasks/Task_4.java
@@ -6,15 +6,16 @@ public class Task_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 1;
-        multiplicationTable(n, num);
+        int m = 1;
+        multiplication_table(n, m);
     }
 
-    public static void multiplicationTable(int n, int num) {
-        if (num > 10) {
+    public static void multiplication_table(int a, int b) {
+        if (b > 10) {
             return;
+        } else {
+            System.out.println(a + " x " + b + " = " + (a * b));
+            multiplication_table(a, b + 1);
         }
-        System.out.println(n + " x " + num + " = " + (n * num));
-        multiplicationTable(n, num + 1);
     }
 }
