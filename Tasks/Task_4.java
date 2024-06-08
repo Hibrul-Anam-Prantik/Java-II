0b362e2e7e0f2032c7dfc94410bdde4d220d013e
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-08 15:41:20 +0600

    task 4 added

--- Tasks/Task_4.java
+++ Tasks/Task_4.java
@@ -1,7 +1,20 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class Task_4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 1;
+        multiplicationTable(n, num);
+    }
 
+    public static void multiplicationTable(int n, int num) {
+        if (num > 10) {
+            return;
+        }
+        System.out.println(n + " x " + num + " = " + (n * num));
+        multiplicationTable(n, num + 1);
     }
 }
