e66d3cd02e8a120a4fc9a482377871c9916c0bbf
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-10 21:45:49 +0600

    class file added

--- OOP/Student.java
+++ OOP/Student.java
@@ -1,10 +1,16 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class Student {
     public String name;
     public int id;
 
     public void profileCreator() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name: ");
+        name = sc.nextLine();
+        System.out.println("Enter Your ID: ");
+        id = sc.nextInt();
     }
 }
