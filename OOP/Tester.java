34042763299fe2482af32ca4bd8909a2ce4bc767
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-11 18:20:00 +0600

    oop tester

--- OOP/Tester.java
+++ OOP/Tester.java
@@ -1,7 +1,15 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        System.err.println("Hello");
+        System.out.print("How many students are there?\n=> ");
+        Scanner sc = new Scanner(System.in);
+        int numSt = sc.nextInt();
+        sc.nextLine();
+        Student s1 = new Student(numSt);
+        s1.profileCreator(numSt);
+        sc.close();
     }
 }
