331f8026e2e30bedcb7a2deb8f233bf1176d6e87
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-11 18:17:01 +0600

    Oop

--- OOP/Student.java
+++ OOP/Student.java
@@ -1,16 +1,29 @@
 package OOP;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Student {
-    public String name;
-    public int id;
+    public String[] names;
+    public int[] ids;
 
-    public void profileCreator() {
+    public Student(int numSt) {
+        names = new String[numSt];
+        ids = new int[numSt];
+    }
+
+    public void profileCreator(int numSt) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Name: ");
-        name = sc.nextLine();
-        System.out.println("Enter Your ID: ");
-        id = sc.nextInt();
+        for (int i = 0; i < numSt; i++) {
+            System.out.println("Student_" + (i + 1) + ":\n============");
+            System.out.print("Enter Name: ");
+            names[i] = sc.nextLine();
+            System.out.print("Enter ID: ");
+            ids[i] = sc.nextInt();
+            sc.nextLine();
+        }
+        System.out.println(Arrays.toString(names));
+        System.out.println(Arrays.toString(ids));
+        sc.close();
     }
 }
