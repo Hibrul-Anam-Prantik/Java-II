b0b0aa54e1a81dabfda19fc4c7669f8e2fcc5bbb
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-13 01:07:31 +0600

    Task 5 added

---
+++ Tasks_LA02/Task3.java
@@ -0,0 +1,5 @@
+package Tasks_LA02;
+
+public class Task3 {
+    
+}
---
+++ Tasks_LA02/Task5.java
@@ -0,0 +1,35 @@
+package Tasks_LA02;
+
+import java.util.Scanner;
+
+public class Task5 
+{
+    public static void main(String[] args)
+    {
+        int zeroCount = 0;
+        int numCount = 0;
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; ; i++){
+            int num = sc.nextInt();
+            sum += num;
+            if(num == 0){
+                zeroCount ++;
+            } else{
+                numCount ++;
+                zeroCount = 0;
+            }
+            if(zeroCount == 3){
+                break;
+            }
+        }
+        if(sum == 0){
+            System.out.println("No numbers found");
+        } else{
+            double avg = (sum*1.0)/numCount;
+            System.out.println("Sum = "+sum);
+            System.out.println("Average  = "+avg);
+        }
+        sc.close();
+    }
+}
