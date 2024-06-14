19cbacc1a67c796f58fa89f6d085f76af57a10be
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-14 23:38:37 +0600

    Task 6 added

---
+++ Tasks_LA02/Task6.java
@@ -0,0 +1,24 @@
+package Tasks_LA02;
+
+import java.util.Scanner;
+
+public class Task6 
+{
+    public static void main(String[] args)
+    {
+        double num;
+        double weight;
+        double sumWeightAndNum = 0;
+        double sumWeight = 0;
+        Scanner sc = new Scanner(System.in);
+        for ( int i = 0; i < 5; i++){
+            num = sc.nextDouble();
+            weight = sc.nextDouble();
+            sumWeightAndNum += (num*weight);
+            sumWeight += weight;
+        }
+        double avg = sumWeightAndNum/sumWeight;
+        System.out.println("Weighted Average = "+ avg);
+        sc.close();
+    }    
+}
