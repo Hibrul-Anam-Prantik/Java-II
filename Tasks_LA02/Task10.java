163754fc493d896b1ac2711fa165f6557a55deb5
Hibrul Anam Prantik <168169689+Hibrul-Anam-Prantik@users.noreply.github.com>
2024-06-15 23:53:33 +0600

    Update Task10.java

--- Tasks_LA02/Task10.java
+++ Tasks_LA02/Task10.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Task10
 {
-public static void main (String [] args)
-{
-Scanner sc = new Scanner (System.in);
+  public static void main (String [] args)
+  {
+    Scanner sc = new Scanner (System.in);
 
-}
+  }
 }
