c9d585421fa10e1e63feac2ca0c65e7cf71c12af
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-18 23:41:54 +0600

    Cat

---
+++ OOP/Cat.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Cat 
+{
+    public String color;
+    public String action;
+    
+    public void details()
+    {
+        System.out.println("The "+color+" cat is "+action);
+    }
+}
---
+++ OOP/TesterCat.java
@@ -0,0 +1,14 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class TesterCat 
+{
+    public static void main(String[] args) 
+    {
+        Cat c1 = new Cat();
+        c1.color = "White";
+        c1.action = "sleeping";
+        c1.details();
+    }    
+}
