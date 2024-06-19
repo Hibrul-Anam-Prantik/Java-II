512b204012040142c87876ff9f25de8037853e31
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-19 21:24:40 +0600

    cat tester udtaded

--- OOP/Cat.java
+++ OOP/Cat.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class Cat 
 {
     public String color;
@@ -8,5 +10,15 @@ public class Cat
     public void details()
     {
         System.out.println("The "+color+" cat is "+action);
+    }
+    public void cat()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the color of the Cat: ");
+        color = sc.nextLine();
+        System.out.print("Enter the action of the Cat: ");
+        action = sc.nextLine();
+        System.out.println("The "+color+" cat is "+action);
+        sc.close();
     }
 }
--- OOP/TesterCat.java
+++ OOP/TesterCat.java
@@ -6,9 +6,11 @@ public class TesterCat
 {
     public static void main(String[] args) 
     {
-        Cat c1 = new Cat();
-        c1.color = "White";
+         Cat c1 = new Cat();
+    /*   c1.color = "White";
         c1.action = "sleeping";
         c1.details();
+        */
+        c1.cat();
     }    
 }
