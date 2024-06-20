6810007ac724b851878fbf5f6541af1614a69107
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-20 23:54:47 +0600

    class tester

---
+++ OOP/ClassTester.java
@@ -0,0 +1,12 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class ClassTester 
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tester check");
+        sc.close();
+    }    
+}
