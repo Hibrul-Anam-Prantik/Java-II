ff3df9ded502a06bcbbab92d68a34d33fd51a234
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-21 23:02:05 +0600

    oop

--- OOP/ClassTester.java
+++ OOP/ClassTester.java
@@ -6,7 +6,24 @@ public class ClassTester
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Tester check");
+        System.out.print("Demo_Class\n\nEnter an String: ");
+        Demo_Class dc = new Demo_Class();
+        String s = dc.anything = sc.nextLine();
+    //    sc.nextInt();
+        System.out.println("OUTPUT");
+        String newS = dc.anyS(s);
+        System.out.println(newS);
         sc.close();
     }    
+}
+
+class Demo_Class
+{
+    public String anything;
+    public int anyNumber;
+
+    String anyS(String s)
+    {
+        return "The String is: " + s + "\nLength of the String: "+s.length();
+    }
 }
