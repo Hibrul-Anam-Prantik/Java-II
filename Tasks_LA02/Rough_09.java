83b8d7b096f864affb970c7137d983d97c58676d
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-22 20:08:05 +0600

    rough 9

---
+++ Tasks_LA02/Rough_09.java
@@ -0,0 +1,31 @@
+package Tasks_LA02;
+
+import java.util.Scanner;
+
+public class Rough_09 
+{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      String s = "" + n;
+      int count = s.length();
+      System.out.println(count + " digits");
+
+      // b 
+      count = s.length();
+      int[] digits = new int[count];
+        
+      // Extract digits and store in array
+      int tempNumber = n;
+      for (int i = count - 1; i >= 0; i--) {
+          digits[i] = tempNumber % 10;
+          tempNumber /= 10;
+      }
+      
+      // Encrypt each digit by multiplying by 7 and print the result
+      for (int i = 0; i < count; i++) {
+          System.out.print(digits[i] * 7 + " ");
+      }
+      sc.close();  
+    } 
+}
