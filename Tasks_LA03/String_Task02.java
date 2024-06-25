1f00a9796359e62ae0081f7ee238197b1ea547f2
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-06-26 03:24:23 +0600

    String task 02 added

---
+++ Tasks_LA03/String_Task02.java
@@ -0,0 +1,30 @@
+package Tasks_LA03;
+
+import java.util.Scanner;
+
+public class String_Task02 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        String str = sc.nextLine();
+        str = str.toLowerCase();  // as stated in the question, the input must be in lower case
+        String newStr = "";
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            int ascii;
+            if(ch == 'a'){    // handling char 'a'
+                ascii = (int)(ch) + 25;
+                ch = (char)(ascii);
+                newStr += ch;
+            } else {        // handling other char
+                ascii = (int)(ch) - 1;
+                ch = (char)(ascii);
+                newStr += ch;
+            }
+        }
+        System.out.println(newStr);
+        sc.close();
+    }    
+}
