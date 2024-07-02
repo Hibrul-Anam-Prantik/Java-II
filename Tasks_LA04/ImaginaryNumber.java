6dfd426ca4c1f4d6a28e29f8836dccc2c9820750
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-02 19:17:24 +0600

    TASK 6

---
+++ Tasks_LA04/ImaginaryNumber.java
@@ -0,0 +1,12 @@
+package Tasks_LA04;
+
+// TASK 6
+public class ImaginaryNumber {
+    public int realPart;
+    public int imaginaryPart;
+
+    void printNumber() 
+    {
+        System.out.println(realPart + " + " + imaginaryPart + "i");
+    }
+}
