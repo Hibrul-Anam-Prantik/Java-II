9f923dbd428cbfb20a6c74daf92915dac90ce3fa
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-02 19:08:29 +0600

    Test 3

--- Tasks_LA04/Test3.java
+++ Tasks_LA04/Test3.java
@@ -5,8 +5,8 @@ public class Test3
     public static void main(String [] args){
         Circle cr = new Circle();
         System.out.println("Radius of the circle is "+ cr.radius); 
-        double pi = 3.1416d;
-        System.out.println("The area of the circle is "+ pi*cr.radius*cr.radius);
-        System.out.println("The circumference of the circle is "+ 2*pi*cr.radius);
+        double pi = Math.PI;
+        System.out.println("The area of the circle is "+ (pi*cr.radius*cr.radius));
+        System.out.println("The circumference of the circle is "+ (2*pi*cr.radius));
     }    
 }
