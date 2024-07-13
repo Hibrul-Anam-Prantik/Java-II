edc95b07c175153bd3b2905192b0127252f4f283
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-14 01:34:56 +0600

    updates

---
+++ Cart.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Cart {
+    public int cNum;    // cart number
+    public String[] item = new String[3];
+    public double[] price = new double[3];
+    public int iI;    // index of items
+    public int iP;    // index of prices
+    public double discount;
+    public void create_cart(int cNum)
+    {
+        this.cNum = cNum;   // cart number
+        iI = 0;    // index of items
+        iP = 0;    // index of prices
+    }
+    public void addItem(String item, double price)
+    {
+        if(iI < 3 && iP < 3) {
+            this.item[iI++] = item;
+            System.out.println(item + " added to cart " + cNum + ".");
+            System.out.println("You have " + iI + " item(s) in your cart now.");
+            this.price[iP++] = price;
+        } else {
+            System.out.println("You already have 3 items on your cart");
+        }
+    }
+    public void cartDetails()
+    {
+        double sum = 0;
+        System.out.println("Your cart(c" + cNum + ") :");
+        for (int i = 0; i < iI; i++) {
+            System.out.println(item[i] + " - " + price[i]);
+            sum += price[i];
+        }
+        System.out.println("Discount Applied: " + discount + "%");
+        if (discount != 0) {
+            double x = sum * (discount/100);   // x = discounted ammount, that is to be deducted from sum;
+            sum -= x;
+        }
+        System.out.println("Total price: " + sum);
+    }
+    public void giveDiscount(double discount)
+    {
+        this.discount = discount;
+    }
+}
--- Task_LA_05/MagicItem.java
+++ Task_LA_05/MagicItem.java
@@ -4,69 +4,70 @@ public class MagicItem
 {
     public String name;
     public int level;
-    public String item1;
-    public String item2;
-    public String item3;
+    public String[] items;
+    int iT;      // to keep a tract of the index of items[]
+    
     public void newCharacter(String name)
     {
         this.name = name;
+        level = 0;
+        items = new String[3];
+        iT = 0;
     }
+
     public void displayInfo()
     {
         System.out.println("Character Name: " + name);
         System.out.println("Energy Level: " + level);
-        System.out.println("Item 1: " + item1);
-        System.out.println("Item 2: " + item2);
-        System.out.println("Item 3: " + item3);
+        for (int i = 0; i < items.length; i++)
+        {
+            System.out.println("Item " + (i+1) + ": " + items[i]);
+        }
     }
+
     public void findItem(String item)
     {
-        if(item1 == null) {
-            item1 = item;
-            System.out.println(name + " found a " + item);
-        } else if(item2 == null) {
-            item2 = item;
-            System.out.println(name + " found a " + item);
-        } else if(item3 == null) {
-            item3 = item;
+        if(iT < items.length) {
+            items[iT++] = item;
             System.out.println(name + " found a " + item);
         } else {
             System.out.println("All item slots occupied.");
         }
     }
+
     public void useItem(String item)
     {
-        if (item.equals("Potion") && (item1.equals("Potion") || item2.equals("Potion") || item3.equals("Potion"))) {
-            level += 50;
-            if(item1.equals(item)) {
-                item1 = null;
-            } else if (item2.equals(item)) {
-                item2 = null;
-            } else if (item3.equals(item)) {
-                item3 = null;
+        boolean found = false;     // to check if the item is in the inventory or not
+        String[] tempItems = new String[3];
+        for(int i = 0; i < items.length; i++)
+        {
+            if(items[i].equals("Potion")){
+                System.out.println(name + " used a " + item);
+                level += 50;
+                items[i] = "null"; // tempItems[i];       
+                found = true;
+                System.out.println("Energy Level after using item: " + level);
+                break;
+            } else if (items[i].equals("Elixir")){
+                System.out.println(name + " used a " + item);
+                level += 100;
+                items[i] = "null"; //tempItems[i];
+                found = true;
+                System.out.println("Energy Level after using item: " + level);
+                break;
+            } else if (items[i].equals("Amulet")){
+                System.out.println(name + " used a " + item);
+                level += 200;
+                items[i] = "null"; //tempItems[i];
+                found = true;
+                System.out.println("Energy Level after using item: " + level);
+                break;
+            } else {
+                found = false;
             }
-        } else if (item.equals("Elixir") && (item1.equals("Elixir") || item2.equals("Elixir") || item3.equals("Elixir")))  {
-            level += 100;
-            if(item.equals(item1)) {
-                item1 = null;
-            } else if (item.equals(item2)) {
-                item2 = null;
-            } else if (item.equals(item3)) {
-                item3 = null;
-            }
-        } else if (item.equals("Amulet") && (item1.equals("Amulet") || item2.equals("Amulet") || item3.equals("Amulet"))) {
-            level += 200;
-            if(item.equals(item1)) {
-                item1 = null;
-            } else if (item.equals(item2)) {
-                item2 = null;
-            } else if (item.equals(item3)) {
-                item3 = null;
-            }
-        } else {
-            System.out.println("Item not in inventory.");
         }
-        System.out.println(name + " used a " + item);
-        System.out.println("Energy Level after using item: " + level);
+        if(!found){
+            System.out.println("Item not in inventory.");
+        } 
     }
 }
