a0e1bb627d8bdc21127e80088c281de00f545521
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-13 11:54:05 +0600

    Task 2 (Dog class) added

--- Task_LA_05/Dog.java
+++ Task_LA_05/Dog.java
@@ -4,26 +4,29 @@ public class Dog
 {
     public String name;
     public String color;
-    public void changeName(String name)
+
+    public void changeName(String name) 
     {
         this.name = name;
     }
-    public String bark()
+
+    public String bark() 
     {
-        if( name != null && color == null ) {
+        if (name != null && color == null) {
             return name + " is barking";
-        } else if(color != null && name == null) {
+        } else if (color != null && name == null) {
             return color + " dog is barking";
-        } else if(color == null && name == null) {
+        } else if (color == null && name == null) {
             return "A dog is barking";
-        } else {
-            return name + " the " + color + " dog is barking";
         }
+        return name + " the " + color + " dog is barking";
+        
     }
-    public void changeColor(String color)
+
+    public void changeColor(String color) 
     {
         this.color = color;
-        if ( name == null ) {
+        if (name == null) {
             System.out.println("This dog is " + color);
         } else {
             System.out.println(name + " is " + color);
