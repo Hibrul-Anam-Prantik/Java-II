c53def2b6565430ed7352b2e23192122855efd7e
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-13 15:54:39 +0600

    Task 6 (Cart) done

--- test.java
+++ test.java
@@ -2,40 +2,21 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter another Number: ");
-        int num2 = sc.nextInt();
-        System.out.println();
-        System.out.print("Summation\n---------\n");
-        sum(num1, num2);
-        System.out.println();
-        System.out.print("Substraction\n------------\n");
-        minus(num1, num2);
-        System.out.println();
-        System.out.print("Multiplication\n--------------\n");
-        multiply(num1, num2);
-        System.out.println();
-        System.out.print("Division\n--------\n");
-        division(num1, num2);
-        sc.close();
+        String s = bark();  // returned => "Alfi is barking"
+        System.out.println(s);
+        // or
+        int a = add(3,4);   // sum = 7
+        // so, a = 7;
+        System.out.print(a);
     }
-
-    public static void sum(int n1, int n2) {
-        System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+    public static String bark(){
+        String s = "Alfi is barking";
+        // System.out.println("Alfi is barking");
+        // return "";
+        return s;
     }
-
-    public static void minus(int n1, int n2) {
-        System.out.println(n1 + " - " + n2 + " = " + (n1 - n2));
-    }
-
-    public static void multiply(int n1, int n2) {
-        System.out.println(n1 + " X " + n2 + " = " + (n1 * n2));
-    }
-
-    public static void division(int n1, int n2) {
-        System.out.println(n1 + " ÷ " + n2 + " = " + ((n1 * 1.0) / n2));
-        // System.out.println(n1 + " ÷ " + n2 + " = " + (n1 / (n2 * 1.0)));
+    public static int add(int x, int y){
+        int sum = x+y;
+        return sum;
     }
 }
