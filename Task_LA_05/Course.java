31a80ddba96a4db4a62fe690cb71448d09d086ee
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-13 11:25:32 +0600

    task 1 added

--- Task_LA_05/Course.java
+++ Task_LA_05/Course.java
@@ -5,15 +5,23 @@ public class Course {
     public String code;
     public int credit;
 
-    public void createCourse(String cName, String code, int credit) {
+    public void createCourse(String cName, String code, int credit) 
+    {
         this.cName = cName;
         this.code = code;
         this.credit = credit;
     }
 
-    public void displayCourse() {
+    public void displayCourse() 
+    {
         System.out.println("Course Name: " + cName);
         System.out.println("Course Code: " + code);
         System.out.println("Course Credit: " + credit);
+    }
+    public void updateCourse(String cName, String code, int credit) 
+    {
+        this.cName = cName;
+        this.code = code;
+        this.credit = credit;
     }
 }
