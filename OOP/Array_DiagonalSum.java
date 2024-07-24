cac08639b7bcd1d9420adee156bf43360d04f920
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-07-24 20:26:25 +0600

    updates

---
+++ OOP/Array_DiagonalSum.java
@@ -0,0 +1,28 @@
+package OOP;
+
+// import java.util.*;
+
+public class Array_DiagonalSum 
+{
+    public static void main(String[] args) 
+    {
+        int[][] arr = {{1,2,3,4,1}, {5,6,7,8,1}, {9,10,11,12,1}, {13,14,15,16,1},{17,18,19,20,1}};
+        ArrayPrinter ap = new ArrayPrinter();
+        ap.print2D(arr);
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            // primary diagonal
+                // i = j ;
+            sum += arr[i][i];  
+            // Secondary Diagonal
+                // i + j = n - 1;
+                // => j = n - 1 - i;
+            if ( i != arr.length - 1 - i ) {    
+                // condition to avoid adding the middle element twice
+                sum += arr[i][arr.length - 1 - i];   
+            }    
+        }
+        System.out.println(sum);
+    }
+}
---
+++ OOP/Search_in_Ssorted_Matrix.java
@@ -0,0 +1,40 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class Search_in_Ssorted_Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = { {10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50} };
+        ArrayPrinter ap = new ArrayPrinter();
+        ap.print2D(matrix);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int key = sc.nextInt();
+        stairCaseSearch(matrix, key);
+        sc.close();
+    }
+
+    public static void stairCaseSearch(int[][] matrix, int key)
+    {
+        int row = 0, col = matrix[0].length - 1;
+        boolean found = false;
+        while ( row < matrix.length && col >= 0 )
+        {
+            if (matrix[row][col] == key ) {
+                System.out.println("\"" + key + "\" found at (" + (row + 1) +", " + (col + 1) + ").");
+                found = true;
+                break;
+            } else if ( key < matrix[row][col] ) {
+                col -- ;
+            } else {   // if ( key > matrix[row][col] ) {
+                row ++;
+            } 
+        }
+        if (!found) {
+            System.out.println("\"" + key + "\" not found.");
+        }
+    }
+}
