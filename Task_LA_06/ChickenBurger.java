ce635c588458029139ed97519669c0dc1d62f060
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-03 06:21:48 +0600

    06 fixed

--- Task_LA_06/ChickenBurger.java
+++ Task_LA_06/ChickenBurger.java
@@ -1,13 +1,15 @@
+package Task_LA_06;
+
 // TASK 04
-public class ChickenBurger
+public class ChickenBurger 
 {
      public String bun = "Sesame";
      public int price = 200;
      public String sauceOption = "Less";
      public String spiceLevel = "Not Set";
-     
+
      public String serveBurger() {
-          if(spiceLevel.equals("Not Set")) {
+          if (spiceLevel.equals("Not Set")) {
                return "Cannot serve now. Customize Spice Level first.";
           } else {
                System.out.println("The burger is being served:-");
@@ -17,9 +19,9 @@ public class ChickenBurger
                return "Spice Level: " + spiceLevel;
           }
      }
-     
+
      public void customizeSpiceLevel(String spiceLevel) {
-          if(spiceLevel.equals("Extreme Jhaal")) {
+          if (spiceLevel.equals("Extreme Jhaal")) {
                System.out.println("This spice level is unavailable.");
           } else {
                this.spiceLevel = spiceLevel;
