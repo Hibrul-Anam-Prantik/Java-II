b3758b8d7de3cdf7c6b4a29be1de6d67cfeaac4f
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-10 10:14:24 +0600

    task 1, 2, 3

---
+++ Practice/TASK_07/ParcelDriver.java
@@ -0,0 +1,69 @@
+package Practice.TASK_07;
+
+public class ParcelDriver {
+    public static void main(String[] args){
+       Parcel p1 = new Parcel();
+       p1.printDetails();
+       p1.name = "Spongebob";
+       p1.printDetails();
+       System.out.println("1**************");
+       Parcel p2 = new Parcel("Bob the Builder");
+       p2.weight = 15;
+       p2.calcFee("Gulshan");
+       p2.printDetails();
+       System.out.println("2**************");
+       p2.addWeight(25);
+       p2.calcFee("Banani");        
+  p2.printDetails();
+       System.out.println("3**************");
+       Parcel p3 = new Parcel("Dora the Explorer", 10);
+       p3.addWeight(15);
+       p3.calcFee("Dhanmondi");
+       p3.printDetails();
+    }
+ }
+ 
+class Parcel 
+{
+   public String name;
+   public int weight;
+   public double total_fee;
+   public double location_charge;
+   
+   public Parcel() {
+
+   }
+
+   public Parcel(String name) {
+      this.name = name;
+   }
+
+   public Parcel( String name, int weight) {
+      this.name = name;
+      this.weight = weight;
+   }
+
+   public void calcFee(String location) {
+      if(location.equals("Dhanmondi")) {
+         location_charge = 50;
+      }
+      if(weight == 0) {
+         total_fee = 0;
+      } else {
+         total_fee = (weight * 20) + location_charge;
+      }
+   }
+
+   public void addWeight(int add) {
+      weight += add;
+      System.out.println("Updated Weight: " + weight);
+   }
+
+   public void printDetails() {
+      if(name == null) {
+         System.out.println("Set name first");
+      } else {
+         System.out.println("Name: " + name + "\nTotal Weight: " + weight + "\nTotal Fee: "  + total_fee);
+      }
+   }
+}
---
+++ Practice/TASK_07/StudentTester.java
@@ -0,0 +1,40 @@
+package Practice.TASK_07;
+
+public class StudentTester{
+    public static void main(String[] args){
+        Student s1 = new Student("Harry", "CSE");
+        System.out.println(s1.name);
+        s1.updateName("Harry Potter");
+        System.out.println(s1.accessName());
+        System.out.println(s1.prog);
+        s1.updateProgram("CS");
+        String prog = s1.accessProgram();
+        System.out.println(prog);
+    }
+ }
+ 
+ class Student 
+ {
+    public String name;
+    public String prog;
+
+    public Student(String name, String prog) {
+        this.name = name;
+        this.prog = prog;
+    }
+    public void updateName(String update) {
+        name = update;
+    }
+
+    public String accessName() {
+        return name;
+    }
+
+    public void updateProgram(String update) {
+        prog = update;
+    }
+
+    public String accessProgram() {
+        return prog;
+    }
+ }
