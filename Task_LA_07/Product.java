a5fc5054c893a4d53852ddf22d1764e8e709e3d3
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-10 12:57:03 +0600

    up

--- Task_LA_07/Product.java
+++ Task_LA_07/Product.java
@@ -1,39 +1,39 @@
-public class Product
-  {
-    private String name;
-    private double price;
-    private int quantity;
+public class Product 
+{
+  private String name;
+  private double price;
+  private int quantity;
 
-    public Product() {
-      name = "Unknown";
-      price = 0.0;
-    }
-
-    public void displayInfo() {
-      System.out.println("Product Name: " + name);
-      System.out.println("Price: $" + price);
-    }
-
-    public Product(String name, double price) {
-      this.name = name;
-      this.price = price;
-    }
-
-    public void setQuantity(int n) {
-      quantity = n;
-    }
-
-    public void displayInfo(boolean b) {
-      System.out.println("Product Name: " + name);
-      System.out.println("Price: $" + price);
-      System.out.println("Quantity: " + quantity);
-    }
-
-    public double getPrice() {
-      return price;
-    }
-
-    public int getQuantity() {
-      return quantity;
-    }
+  public Product() {
+    name = "Unknown";
+    price = 0.0;
   }
+
+  public void displayInfo() {
+    System.out.println("Product Name: " + name);
+    System.out.println("Price: $" + price);
+  }
+
+  public Product(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public void setQuantity(int n) {
+    quantity = n;
+  }
+
+  public void displayInfo(boolean b) {
+    System.out.println("Product Name: " + name);
+    System.out.println("Price: $" + price);
+    System.out.println("Quantity: " + quantity);
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+}
