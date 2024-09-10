8fbb997f0ea5ce675e0121ca1d99826709a3b5b6
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-10 07:18:01 +0600

    Udate Task 7

--- Task_LA_06/Course2.java
+++ Task_LA_06/Course2.java
@@ -1,32 +1,38 @@
 package Task_LA_06;
 
 // TASK 07
-// The Difference between Task 6  & Task 7 is that, in Task 7, we face the term Method over loading for the "addContent()" method; one has a single argument & another one has a double argument.
+/*   The Difference between Task 6  & Task 7 is that, in Task 7, 
+     we face the term Method over loading for the "addContent()" method; 
+     one has a single parameter & another one has two parameters.
+*/
 public class Course2 
 {
      public String courseName;
      public String courseCode;
      public String[] syllabus;
      public int contents;
-     int index = 0;     // To access the index of the syllabus[] array
-     
-     public void createCourse(String name, String code) {
+     int index = 0; // To access the index of the syllabus[] array
+
+     public void createCourse(String name, String code) 
+     {
           courseName = name;
           courseCode = code;
           syllabus = new String[4];
      }
-     public void printDetails() {
+
+     public void printDetails() 
+     {
           System.out.println("Course details:");
           System.out.println("Course Name: " + courseName);
           System.out.println("Course Code: " + courseCode);
           System.out.println("Course Syllabus:");
-          if(contents > 0) {
-               if(contents == 1) {
+          if (contents > 0) {
+               if (contents == 1) { // if one content, then "," won't be used
                     System.out.print(syllabus[0]);
                     System.out.println();
-               } else {
-                    for(int i = 0; i < contents - 1; i++) {
-                         
+               } else { // handling ","
+                    for (int i = 0; i < contents - 1; i++) {
+
                          System.out.print(syllabus[i] + ", ");
                     }
                     System.out.println(syllabus[contents - 1]);
@@ -35,29 +41,40 @@ public class Course2
                System.out.println("No content yet.");
           }
      }
-     
-     public void addContent(String syllabus) {
-          
-          if ((contents + 1) <= 4){
-               this.syllabus[index ++] = syllabus;
+
+     public void addContent(String syllabus) 
+     {
+          if ((contents + 1) <= 4) { // 4 = max contents of syllabus
+               this.syllabus[index++] = syllabus;
                System.out.println(syllabus + " was added.");
-               contents ++;
+               contents++;
           } else {
                System.out.println("Cannot add more content");
           }
      }
-     
-     public void addContent(String s1, String s2) {
-          if ((contents+1) <= 4){
-               this.syllabus[index ++] = s1;
-               System.out.println(s1 + " was added.");
-               contents ++;
+
+     public void addContent(String s1, String s2) 
+     {
+          for (int i = 0; i < 2; i++) 
+          {
+               if ((contents + 1) <= 4) {
+                    this.syllabus[index++] = s1;
+                    System.out.println(s1 + " was added.");
+                    contents++;
+                     
+               } else if (contents == 4) {
+                    System.out.println("Cannot add more content");
+                    break;
+               } else {
+                    this.syllabus[index ++] = s2;
+                    System.out.println(s2 + " was added.");
+                    contents ++;
+               }
           }
-          if ((contents+1) <= 4){
-               this.syllabus[index ++] = s2;
-               System.out.println(s2 + " was added.");
-               contents ++;
-               
-          }  
+          // if ((contents+1) <= 4){
+          // this.syllabus[index ++] = s2;
+          // System.out.println(s2 + " was added.");
+          // contents ++;
+          // }
      }
 }
