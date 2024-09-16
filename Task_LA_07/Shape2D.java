8d0c308d7f03a4c85a82b520a9416c6a71c5346d
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-16 19:08:08 +0600

    fix

--- Task_LA_07/Shape2D.java
+++ Task_LA_07/Shape2D.java
@@ -20,21 +20,15 @@ public class Shape2D {
     area = 0.5 * (base * height);
   }
 
-  // boolean sides; /*
-  // * I will use this flag to determine whether the triangle has sides or not,
-  // * if the triangle has sides I will use the "printf" to print the area.
-  // */
-
   public Shape2D(int a, int b, int c) {
-    // sides = true;
     shape = "Triangle";
-    System.out.println("A " + shape + "  has been created withthe following sides: " + a + ", " + b + ", " + c);
+    System.out.println("A " + shape + "  has been created with the following sides: " + a + ", " + b + ", " + c);
     double s = 0.5 * (a + b + c); // halfPerimeter
     double x = s * (s - a) * (s - b) * (s - c);
     area = Math.sqrt(x);
   }
 
-  public void area() {
-    System.out.println("The area of the " + shape + " is: " + area);
+  public void area() { 
+    System.out.println("The area of the " + shape + " is: " + area); 
   }
 }
