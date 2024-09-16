acd6b390ee7de370fb3c1077c2c27d6268c6b10c
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-16 15:31:46 +0600

    up

--- Task_LA_07/TASK07/Student.java
+++ Task_LA_07/TASK07/Student.java
@@ -1,4 +1,74 @@
 package TASK07;
 public class Student {
+    private double cg;
+    private String course;
+    private String courses[];
+    private int id;
+    private int courseCount;
+    private int idx;    // index for adding courses to the array
     
+    public Student(int id) {
+        this.id = id;
+        courses = new String[4];
+    }
+
+    public Student(int id, double cg) {
+        this.id = id; this.cg = cg;
+    }
+    
+    public void addCourse(String course) {
+        if(cg == 0) {
+            System.out.println("Failed to add " + course + "\nSet CG first");
+        } else {
+            if(courseCount >= 3 && cg < 3) {
+                System.out.println("Failed to add " + course);
+                System.out.println("CG is low. Can't add more than 3 courses.");
+            } else {
+                courses[idx++] = course;
+                courseCount ++;
+            }
+        }
+    }
+
+    public void setCG(double cg) {
+        this.cg = cg;
+    }
+
+    public void showAdvisee() {
+        System.out.println("Student ID: " + id + ", CGPA: " + cg);
+        if(courseCount == 0) {
+            System.out.println("No courses added.");
+        } else {  
+            System.out.println("Added courses are:");
+            for(int i = 0; i < courseCount; i++) {
+                System.out.print(courses[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void rmAllCourse() {
+        courseCount = 0; idx = 0;
+        courses = new String[4];
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void addCourse(String[] courses) {
+        for(int i = 0; i < courses.length; i++) {
+            if(courseCount >= 3 && cg < 3) {
+                System.out.println("Failed to add " + courses[i]);
+                System.out.println("CG is low. Can't add more than 3 courses.");
+                break;
+            } else if(courseCount >= 4) {
+                System.out.println("Failed to add " + courses[i] + "\nMaximum 4 courses allowed.");
+                break;
+            } else {
+                this.courses[idx++] = courses[i];
+                courseCount ++;
+            }
+        }
+    }
 }
