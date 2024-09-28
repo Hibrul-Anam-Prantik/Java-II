b94999560832415dfcf95cc9c731cb71f6daed08
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-28 18:21:17 +0600

    Lab09 > task 2

---
+++ Task_LA_09/Vehicle2010User.java
@@ -0,0 +1,46 @@
+package Task_LA_09;
+
+public class Vehicle2010User
+{
+    public static void main(String[] args){
+        Vehicle2010 car1 = new Vehicle2010();
+        car1.position();
+        car1.moveLowerLeft();
+        car1.position();
+
+        Vehicle2010 car2 = new Vehicle2010();
+        car2.position();
+        car2.moveUpperRight();
+        car2.position();
+        car2.moveLowerRight();
+        car2.position();
+    }
+}
+
+class Vehicle{
+    public int x;
+    public int y;
+
+    public void moveUp(){
+        y = y+1;
+    }
+    public void moveDown(){
+        y = y-1;
+    }
+    public void moveLeft(){
+        x = x-1;
+    }
+    public void moveRight(){
+        x = x+1;
+    }
+    public void position(){
+        System.out.println("("+ x + ","+ y + ")");
+    }
+}
+
+class Vehicle2010 extends Vehicle
+{
+    void moveLowerLeft() {moveDown();moveLeft();}
+    void moveUpperRight() {moveUp(); moveRight();}
+    void moveLowerRight() {moveDown(); moveRight();}
+}
