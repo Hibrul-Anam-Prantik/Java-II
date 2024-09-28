5059659b87a9686623a4a43e8414c3333881f0b5
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-28 11:44:44 +0600

    Lab09 > task 3

---
+++ Task_LA_09/TestAccount.java
@@ -0,0 +1,41 @@
+package Task_LA_09;
+
+public class TestAccount
+{
+    public static void main(String [] args){
+        System.out.println("Total Checking Accounts: "+CheckingAccount.count);
+        CheckingAccount c1 = new CheckingAccount();
+        System.out.println("Account Balance: " + c1.showBalance());
+        CheckingAccount c2 = new CheckingAccount(100.0);
+        System.out.println("Account Balance: " + c2.showBalance());
+        CheckingAccount c3 = new CheckingAccount(200.0);
+        System.out.println("Account Balance: " + c3.showBalance());
+        System.out.println("Total Checking Accounts: "+CheckingAccount.count);
+    }
+}
+
+class Account{
+    public double balance = 0.0;
+
+    public Account(double balance){
+        this.balance = balance;
+    }
+    public double showBalance(){
+        return balance;
+    }
+}
+
+class CheckingAccount extends Account
+{
+    public static int count = 0;
+
+    CheckingAccount () {
+        super(0.0);
+        count +=1;
+    }
+
+    CheckingAccount(double balance) {
+        super(balance);
+        count += 1;
+    }
+}
