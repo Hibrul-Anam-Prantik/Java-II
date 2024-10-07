7ed763a89618f13d22245a263ca34afc411b5f3a
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-07 15:41:04 +0600

    Exception Handling > Try Catch Block

---
+++ ExceptionHandling/TryCatchBlock.java
@@ -0,0 +1,22 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+public class TryCatchBlock
+{
+    TryCatchBlock(String str) {
+        try {
+            int i = Integer.parseInt(str);
+            System.out.println(i);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        sc.close();
+        TryCatchBlock tcb = new TryCatchBlock(S);
+    }
+}
